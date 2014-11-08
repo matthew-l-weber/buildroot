18b4bbcc42a6a4ade50869a936faa076705abbd7
Matt Weber <matthew.l.weber@gmail.com>
2014-11-08 03:18:12 -0600

    fixed mkcard nameing

--- board/beaglebone/post-image.sh
+++ board/beaglebone/post-image.sh
@@ -4,6 +4,6 @@
 
 # copy the uEnv.txt to the output/images directory
 cp board/beaglebone/uEnv.txt $BINARIES_DIR/uEnv.txt
-cp board/beaglebone/mkCard.sh $BINARIES_DIR/
+cp board/beaglebone/mkcard.sh $BINARIES_DIR/
 cp board/beaglebone/loadCard.sh $BINARIES_DIR/
-chmod 700 $BINARIES_DIR/mkCard.sh $BINARIES_DIR/loadCard.sh
+chmod 700 $BINARIES_DIR/mkcard.sh $BINARIES_DIR/loadCard.sh
