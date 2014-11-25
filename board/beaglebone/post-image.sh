da1f27aa20f3141aa13714157e0f3b1920f169fe
Matt Weber <matthew.l.weber@gmail.com>
2014-11-25 10:33:52 -0600

    prototyped overlay dtbo generation.

--- board/beaglebone/post-image.sh
+++ board/beaglebone/post-image.sh
@@ -7,3 +7,7 @@ cp board/beaglebone/uEnv.txt $BINARIES_DIR/uEnv.txt
 cp board/beaglebone/mkcard.sh $BINARIES_DIR/
 cp board/beaglebone/loadCard.sh $BINARIES_DIR/
 chmod 700 $BINARIES_DIR/mkcard.sh $BINARIES_DIR/loadCard.sh
+
+# Gen overlays
+#board/beaglebone/dtc -O dtb -o $BINARIES_DIR/v1_bbb_comforStat.dtbo -b O -@ board/beaglebone/v1_bbb_comforStat.dts
+
